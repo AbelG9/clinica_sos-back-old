4b04e75afbdbfc9067ad2fef2fb30b98a8b74b39
AbelG9 <55357845+AbelG9@users.noreply.github.com>
2020-07-21 21:52:27 -0500

    guardado de citas corregido

--- app/Http/Controllers/CitasController.php
+++ app/Http/Controllers/CitasController.php
@@ -14,10 +14,10 @@ class CitasController extends Controller
         $CitaOnline->id_usuario = 33;
         // $CitaOnline->cme_paciente_fullname = "";
         // $CitaOnline->cme_color = "";
-        $CitaOnline->cme_fech_inicial = $request->datoscitaSave['fech_inicial'];
+        $CitaOnline->cme_fech_inicial = $request->datoscita['hora_inicial'];
         // $CitaOnline->cme_fech_final = "";
         // $CitaOnline->cme_obs="";
-        $CitaOnline->cme_titulo = $request->datoscitaSave['titulo'];
+        $CitaOnline->cme_titulo = $request->datoscita['motivo'];
         $CitaOnline->id_especialidad = 1;
         // $CitaOnline->cme_attention_pes="";
         // $CitaOnline->cme_us_responsable="";
@@ -26,6 +26,7 @@ class CitasController extends Controller
         // $CitaOnline->cme_estado = "abierto";
         // $CitaOnline->cme_esp_ref = "";
         // $CitaOnline->cme_manual = "";
+        $CitaOnline->cme_agenda_cita = "";
         $CitaOnline->save();
         
         return $CitaOnline->id_cita_medica;
--- app/Model/Citas.php
+++ app/Model/Citas.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Citas extends Model
 {
-    public $timestamps = false;
+    public $timestamps = true;
     protected $primaryKey = 'id_cita_medica';
     protected $table = 'cita_medica';
     const CREATED_AT = 'cme_fech_register';
