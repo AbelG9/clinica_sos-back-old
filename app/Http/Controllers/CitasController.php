c743f32cdd7955bf1d2d2f2baac25ab845eae7a5
VicCham98 <41102257+VicCham98@users.noreply.github.com>
2020-07-22 11:59:54 -0500

    savecitaOnline modified
    modulo citas

--- app/Http/Controllers/CitasController.php
+++ app/Http/Controllers/CitasController.php
@@ -3,32 +3,45 @@
 namespace App\Http\Controllers;
 
 use App\Model\Citas;
+use App\Model\Patient;
+use App\Model\Patient_especiality;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class CitasController extends Controller
 {
     public function saveCitaOnline(Request $request)
     {
-        $CitaOnline = new Citas;
-        $CitaOnline->id_paciente = 1000;
-        $CitaOnline->id_usuario = 33;
-        // $CitaOnline->cme_paciente_fullname = "";
-        // $CitaOnline->cme_color = "";
-        $CitaOnline->cme_fech_inicial = $request->datoscita['hora_inicial'];
-        // $CitaOnline->cme_fech_final = "";
-        // $CitaOnline->cme_obs="";
-        $CitaOnline->cme_titulo = $request->datoscita['motivo'];
-        $CitaOnline->id_especialidad = 1;
-        // $CitaOnline->cme_attention_pes="";
-        // $CitaOnline->cme_us_responsable="";
-        // $CitaOnline->cme_tratamiento="";
-        // $CitaOnline->cme_trat_descripcion="";
-        // $CitaOnline->cme_estado = "abierto";
-        // $CitaOnline->cme_esp_ref = "";
-        // $CitaOnline->cme_manual = "";
-        $CitaOnline->cme_agenda_cita = "";
-        $CitaOnline->save();
-        
-        return $CitaOnline->id_cita_medica;
+
+        $pacienteESP = Patient_especiality::where('id_paciente', '=', $request->datoscita['paciente_id_paciente'])
+            ->select('id_paciente_especialidad')
+            ->get();
+        $paciente = Patient::where('id_paciente', '=', $request->datoscita['paciente_id_paciente'])
+            ->select('pac_name', 'pac_lastname')
+            ->get();
+
+        if (count($pacienteESP) > 0 && count($paciente) > 0) {
+            $CitaOnline = new Citas;
+            $CitaOnline->id_paciente = $request->datoscita['paciente_id_paciente'];
+            $CitaOnline->id_usuario = 19;
+            $CitaOnline->cme_paciente_fullname = $paciente[0]->pac_name." ".$paciente[0]->pac_lastname;
+            $CitaOnline->cme_color = "#d94848";
+            $CitaOnline->cme_fech_inicial = $request->datoscita['hora_inicial'];
+            // $CitaOnline->cme_fech_final = "";
+            // $CitaOnline->cme_obs="";
+            $CitaOnline->cme_titulo = "WEB - ".$request->datoscita['motivo'];
+            $CitaOnline->id_especialidad = 1;
+            $CitaOnline->cme_attention_pes= $pacienteESP[0]->id_paciente_especialidad;
+            $CitaOnline->cme_us_responsable="10";
+            // $CitaOnline->cme_tratamiento="";
+            // $CitaOnline->cme_trat_descripcion="";
+            $CitaOnline->cme_estado = "abierto";
+            $CitaOnline->cme_esp_ref = "Dr. Prueba";
+            $CitaOnline->cme_manual = "1";
+            $CitaOnline->cme_agenda_cita = "";
+            $CitaOnline->save();
+
+            return $CitaOnline->id_cita_medica;
+        }
     }
 }
