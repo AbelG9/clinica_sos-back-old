b51a8f676d81d7462516a9cdb5c1d407ff24573c
VicCham98 <41102257+VicCham98@users.noreply.github.com>
2020-07-29 21:22:15 -0500

    function complete task
    modulo tarea

--- app/Http/Controllers/GiveTaskController.php
+++ app/Http/Controllers/GiveTaskController.php
@@ -7,6 +7,8 @@ use Carbon\Carbon;
 use App\Model\GiveTask;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
 
 class GiveTaskController extends Controller
 {
@@ -93,8 +95,11 @@ class GiveTaskController extends Controller
 
     public function getTask (Request $request) {
         $task = GiveTask::where('id', '=', $request->id)
-            ->select('id', 'asunto', 'detalle', 'created_at', 'fechafin', 'horafin', 'estado', 'fecha_entrega', 'trabajo')
+            ->select('id', 'asunto', 'detalle', 'created_at', 'fechafin', 'horafin', 'estado', 'fecha_entrega', 'trabajo', 'trabajo_file', 'comentario')
             ->first();
+        $pathFile = $task->trabajo_file;
+        $url = url('/')."/public".Storage::url("trabajos/".$pathFile);
+        $task->file_url = $url;
         return response()->json([
             'success' => true,
             'task' => $task,
@@ -103,15 +108,112 @@ class GiveTaskController extends Controller
     }
 
     public function completeTask (Request $request) {
-        $task = GiveTask::find($request->id);
-        $task->trabajo = $request->data['enlace'];
-        $task->comentario = $request->data['comentario'];
-        $task->fecha_entrega = Carbon::now()->format('Y-m-d H:m:s');
-        $task->estado = 'COMPLETADO';
-        $task->save();
-        return response()->json([
-            'success' => true,
-            'message' => 'Success'
-        ]);
+        $size_in_bytes  = (int) (strlen(rtrim($request->data['file_name'], '=')) * 3 / 4);
+        $size_in_kb    = $size_in_bytes  / 1024;
+        // $size_in_mb    = $size_in_kb / 1024;
+
+        if ($size_in_kb <= 10000) {
+            $extension = explode('/', mime_content_type($request->data['file_name']));
+            if ($extension[1] === 'pdf' || $extension[0] === 'image') {
+                $fileName = time().'.'.$extension[1];
+                $format = "data:".$extension[0]."/".$extension[1].";base64,";
+                $file = str_replace($format, '', $request->data['file_name']);
+                $file = str_replace(" ", "+", $file);
+                $file = base64_decode($file);
+                Storage::disk('public')->put("trabajos/${fileName}", $file);
+
+                $exists = Storage::disk('public')->exists("trabajos/${fileName}");
+                if ($exists) {
+                    $task = GiveTask::find($request->id);
+                    $task->trabajo = $request->data['enlace'];
+                    $task->trabajo_file = $fileName;
+                    $task->comentario = $request->data['comentario'];
+                    $task->fecha_entrega = Carbon::now()->format('Y-m-d H:m:s');
+                    $task->estado = 'COMPLETADO';
+                    $task->save();
+                    $url = Storage::url("trabajos/${fileName}");
+                    // $types = $this->mimeTypesAll();
+                    return response()->json([
+                        'success' => true,
+                        'path' => $url,
+                        'message' => 'Uploaded successfully'
+                    ]);
+                } else {
+                    return response()->json([
+                        'success' => false,
+                        'path' => $fileName,
+                        'message' => 'Failed to upload file'
+                    ]);
+                }
+            } else {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'File not supported!'
+                ]);
+            }
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'File excced the limit!'
+            ]);
+        }
+    }
+
+    private function mimeTypesAll () {
+        $mime_types = array(
+            'txt' => 'text/plain',
+            'htm' => 'text/html',
+            'html' => 'text/html',
+            'php' => 'text/html',
+            'css' => 'text/css',
+            'js' => 'application/javascript',
+            'json' => 'application/json',
+            'xml' => 'application/xml',
+            'swf' => 'application/x-shockwave-flash',
+            'flv' => 'video/x-flv',
+
+            // images
+            'png' => 'image/png',
+            'jpe' => 'image/jpeg',
+            'jpeg' => 'image/jpeg',
+            'jpg' => 'image/jpeg',
+            'gif' => 'image/gif',
+            'bmp' => 'image/bmp',
+            'ico' => 'image/vnd.microsoft.icon',
+            'tiff' => 'image/tiff',
+            'tif' => 'image/tiff',
+            'svg' => 'image/svg+xml',
+            'svgz' => 'image/svg+xml',
+
+            // archives
+            'zip' => 'application/zip',
+            'rar' => 'application/x-rar-compressed',
+            'exe' => 'application/x-msdownload',
+            'msi' => 'application/x-msdownload',
+            'cab' => 'application/vnd.ms-cab-compressed',
+
+            // audio/video
+            'mp3' => 'audio/mpeg',
+            'qt' => 'video/quicktime',
+            'mov' => 'video/quicktime',
+
+            // adobe
+            'pdf' => 'application/pdf',
+            'psd' => 'image/vnd.adobe.photoshop',
+            'ai' => 'application/postscript',
+            'eps' => 'application/postscript',
+            'ps' => 'application/postscript',
+
+            // ms office
+            'doc' => 'application/msword',
+            'rtf' => 'application/rtf',
+            'xls' => 'application/vnd.ms-excel',
+            'ppt' => 'application/vnd.ms-powerpoint',
+
+            // open office
+            'odt' => 'application/vnd.oasis.opendocument.text',
+            'ods' => 'application/vnd.oasis.opendocument.spreadsheet',
+        );
+        return $mime_types;
     }
 }
